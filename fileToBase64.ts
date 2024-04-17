5e758efbd9814a2468daf77c5ac4e4be44aafaac
Victor Cabral <107046713+victorcabral182@users.noreply.github.com>
2024-04-17 10:26:17 -0300

    Create fileToBase64.ts

---
+++ fileToBase64.ts
@@ -0,0 +1,10 @@
+  function fileToBase64(file: any) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  }
